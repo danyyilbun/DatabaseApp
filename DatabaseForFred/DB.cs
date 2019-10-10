9d7222aff8b9c429d388d0b72dca8b96b1568032
danyyilbun <48656385+danyyilbun@users.noreply.github.com>
2019-10-10 15:07:12 -0400

    Add project files.

---
+++ DatabaseForFred/DB.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseForFred
+{
+    public class DB
+    {
+        private static Demo1Entities myDb = new Demo1Entities();
+
+        public static IEnumerable<Product> GetProduct()
+        {
+            return myDb.Products.ToList();
+        }
+        public static Product GetProduct(int id)
+        {
+            return myDb.Products.Find(id);
+        }
+
+    }
+}
